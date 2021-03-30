5caf82ffebf4cb80a1c4a920a1bca539ce84d750
Muhammet Yağız <64231904+myagiz@users.noreply.github.com>
2021-03-30 03:08:53 +0300

    working on validation

---
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.DynamicProxy;
+using Core.CrossCuttingCorners.Validation;
+using FluentValidation;
+
+namespace Core.Aspects.Autofac.Validation
+{
+    public class ValidationAspect
+    {
+        private Type _validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            //defensive coding
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+            {
+                throw new System.Exception("Bu bir doğrulama sınıfı değil");
+            }
+
+            _validatorType = validatorType;
+        }
+       
+    }
+}
